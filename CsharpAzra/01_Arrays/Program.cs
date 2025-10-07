ce70a153fb527958916a65c6a1cf3ad8747e4180
erkanturk <refres98@gmail.com>
2025-10-07 20:20:06 +0300

    array

--- CsharpAzra/01_Arrays/Program.cs
+++ CsharpAzra/01_Arrays/Program.cs
@@ -8,6 +8,12 @@
             //tek toplam çift toplam olarak ekrana yazdıralım.
 
             // //kullanıcının girdiği kelimeyi karakterlere ayırıp gösteren bir dizi yapısı yapalım.
+            Console.WriteLine("Değer giriniz");
+            string deger = Console.ReadLine();
+            for (int i = 0; i < deger.Length; i++)
+            {
+                Console.WriteLine(deger[i]);
+            }
         }
     }
 }
